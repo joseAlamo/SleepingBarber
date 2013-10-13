50e413c15743f5c415faf5f29fb22228a532bd81
joseAlamo <jsmgl.alamo@gmail.com>
2013-10-13 02:47:36 -0700

    Version 1.0
    Working Solution, doubts on it's correct behavior.

---
+++ SleepingBarber/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SleepingBarber
+{
+    class Program
+    {
+        public static Queue<int> customers = new Queue<int>();
+
+        static void Main(string[] args)
+        {
+            Thread PaulMitchell = new Thread(Barber);
+            Thread Person = new Thread(Customer);
+            Person.Start();
+            PaulMitchell.Start();   
+        }
+
+        private static void Barber()
+        {
+            while (true)
+            {
+                Thread.Sleep(500);
+                if (customers.Count > 0)
+                {
+                    Console.WriteLine("I must wake up and cut {0} heads", customers.Count);
+                    lock (customers)
+                    {
+                        customers.Dequeue();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("I'm Sleeping ZZZZ");
+                }
+            }
+        }
+
+        private static void Customer()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                if (customers.Count < 5)
+                {
+                    Console.WriteLine("My turn, {0} people left", customers.Count);
+                    lock (customers)
+                    {
+                        customers.Enqueue(1);
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Too many people, I'll come back later");
+                }
+               
+            }
+        }
+    }
+}
